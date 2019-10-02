7aeda517b91019c09ae256e639916007859c78d0
shachard46 <shachadavid2013@gmail.com>
2019-10-02 17:35:02 +0300

    cccccccccccccccccccccccccccccc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -37,7 +37,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 
-@TeleOp(name = "test", group = "Linear Opmode")
+@TeleOp(name = "test 1 motor", group = "Linear Opmode")
 public class test extends LinearOpMode {
 
     // Declare OpMode members.
