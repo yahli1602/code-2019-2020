23a119a2db646efbe81661537a84aab46eb1baf8
Ftc-EmekHefer11226 <ftc.emekhefer@gmail.com>
2019-10-17 12:20:52 +0300

    Dont touch autonumus

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aotonomus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aotonomus.java
@@ -13,10 +13,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
-@TeleOp(name = "Vuforia 11226", group = "Concept")
-public class vuforiaOp11226 extends LinearOpMode{
-    @Override
-    public void runOpMode() throws InterruptedException {
+@TeleOp(name = "aotonomus 11226", group = "Concept")
+public class aotonomus {
+
+    public void degreesToTurn () throws InterruptedException {
         VuforiaLocalizer.Parameters prams = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
         prams.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         prams.vuforiaLicenseKey = "AaccUNH/////AAABmYn6tLCgBE4CqSKq7IgTGXZ12c4zYDZx4hC67z9E4/R6MU+miAuEK7eHb/uLBUKMWc3BjFb6o60eLN2HX+BwNNH7qG954X5k3pZyYb9MkYQsAaZ/IYv3S6+JSXLDpQFuNXM7HpZCHuxdN1TwFli4SYBLp7//JhheDx1N0xWeowWQMP3WuTQnbA8TnUpaug3H7liINUkllw/wJcGDVnUjlhUdpHODdC2cptlTol8STSYN3oZQvgXcW+xJErdVnqS6Wev3WnIPelnYRZXxL1Ui9qJn49C18cQJdZ1duV26nfzJ2UyuR7tGUKTJzPjDVkbfU3z96goS+bvA08r2sjHgFh1wcdgPKMSsRMcASTf7u7PQ";
@@ -54,7 +54,7 @@ public class vuforiaOp11226 extends LinearOpMode{
         VuforiaTrackable rear2 = targetsSkyStone.get(12);
         rear2.setName("Rear Perimeter 2");
 
-        waitForStart();
+
 
         targetsSkyStone.activate();
 
@@ -65,16 +65,16 @@ public class vuforiaOp11226 extends LinearOpMode{
                 if(pose != null){
                     VectorF translation = pose.getTranslation();
 
-                    telemetry.addData(beac.getName() + "-Tranlation" , translation);
+
 
                     double degreesToTurn = Math.toDegrees(Math.atan2(translation.get(1), translation.get(2)));
 
-                    telemetry.addData(beac.getName() + "-Degrees",degreesToTurn);
-                    telemetry.update();
+
                 }
             }
-            telemetry.update();
+
         }
 
+        return 0;
     }
 }
