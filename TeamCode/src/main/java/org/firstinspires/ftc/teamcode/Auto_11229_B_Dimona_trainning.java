7a7363992199bd69d09f24b4ed510c4edf858830
Ftc-EmekHefer11226 <ftc.emekhefer@gmail.com>
2019-12-11 14:50:28 +0200

    teleop 11229 and PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_11229_B_Dimona_trainning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_11229_B_Dimona_trainning.java
@@ -5,10 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name = "Auto 11229 B Dimona left turn", group = "Autonomous")
-public class Auto_11229_B_Dimona_turn extends LinearOpMode {
+@Autonomous(name = "Auto 11229 B Dimona Trainning", group = "Autonomous")
+public class Auto_11229_B_Dimona_trainning extends LinearOpMode {
 
     ElapsedTime elapsedTime = new ElapsedTime();
     public DcMotor rdrive1 = null;
@@ -17,6 +18,8 @@ public class Auto_11229_B_Dimona_turn extends LinearOpMode {
     public DcMotor ldrive2 = null;
     public DcMotor slide = null;
     public DcMotor elevator = null;
+    public Servo collectRight = null;
+    public Servo collectLeft = null;
 
 
 
@@ -28,6 +31,8 @@ public class Auto_11229_B_Dimona_turn extends LinearOpMode {
         ldrive2 = hardwareMap.get(DcMotor.class, "lDrive2");
         slide = hardwareMap.get(DcMotor.class, "slide");
         elevator = hardwareMap.get(DcMotor.class, "elevator");
+        collectRight = hardwareMap.get(Servo.class, "collectRight");
+        collectLeft = hardwareMap.get(Servo.class, "collectLeft");
 
         rdrive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rdrive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -55,35 +60,14 @@ public class Auto_11229_B_Dimona_turn extends LinearOpMode {
         ldrive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         ldrive2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        int x = 0;
+int x = 0;
         while (opModeIsActive() && x==0) {
 
             rdrive1.setPower(0.7);
             rdrive2.setPower(0.7);
             ldrive1.setPower(1);
             ldrive2.setPower(1);
-            sleep(600);
-            rdrive1.setPower(0);
-            rdrive2.setPower(0);
-            ldrive1.setPower(0);
-            ldrive2.setPower(0);
-            sleep(100);
-            rdrive1.setPower(0.5);
-            rdrive2.setPower(0.5);
-            ldrive1.setPower(-0.5);
-            ldrive2.setPower(-0.5);
-            sleep(400);
-
-            rdrive1.setPower(0);
-            rdrive2.setPower(0);
-            ldrive1.setPower(0);
-            ldrive2.setPower(0);
-            sleep(100);
-            rdrive1.setPower(1);
-            rdrive2.setPower(1);
-            ldrive1.setPower(1);
-            ldrive2.setPower(1);
-            sleep(450);
+            sleep(500);
             rdrive1.setPower(0);
             rdrive2.setPower(0);
             ldrive1.setPower(0);
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_11229_sensitive.java
@@ -0,0 +1,222 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+@TeleOp(name = "Teleop_11229", group = "Linear Opmode")
+
+public class Teleop_11229_sensitive extends LinearOpMode {
+//private Funcs_11229 funcs = new Funcs_11229();
+
+    //driving motors
+    private DcMotor rDrive1 = null;
+    private DcMotor rDrive2 = null;
+    private DcMotor lDrive1 = null;
+    private DcMotor lDrive2 = null;
+    private DcMotor slide = null;
+    //elevator
+    private DcMotor elevator = null;
+    //fold collection
+    private DcMotor foldcollect = null;
+    //collection
+    private Servo collectRight = null;
+    private Servo collectLeft = null;
+    //grabbing the build plate
+    private Servo grabber1 = null;
+    private Servo grabber2 = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        rDrive1 = hardwareMap.get(DcMotor.class, "rDrive1");
+        rDrive2 = hardwareMap.get(DcMotor.class, "rDrive2");
+        lDrive1 = hardwareMap.get(DcMotor.class, "lDrive1");
+        lDrive2 = hardwareMap.get(DcMotor.class, "lDrive2");
+        slide = hardwareMap.get(DcMotor.class, "slide");
+        elevator = hardwareMap.get(DcMotor.class, "elevator");
+        foldcollect = hardwareMap.get(DcMotor.class, "foldCollect");
+        collectRight = hardwareMap.get(Servo.class, "collectRight");
+        collectLeft = hardwareMap.get(Servo.class, "collectLeft");
+        grabber1 = hardwareMap.get(Servo.class, "grabber1");
+        grabber2 = hardwareMap.get(Servo.class, "grabber2");
+        waitForStart();
+        rDrive1.setDirection(DcMotor.Direction.REVERSE);
+        rDrive2.setDirection(DcMotor.Direction.REVERSE);
+        lDrive1.setDirection(DcMotor.Direction.FORWARD);
+        lDrive2.setDirection(DcMotor.Direction.FORWARD);
+        slide.setDirection(DcMotor.Direction.FORWARD);
+        elevator.setDirection(DcMotor.Direction.FORWARD);
+        foldcollect.setDirection(DcMotor.Direction.FORWARD);
+        grabber1.setDirection(Servo.Direction.FORWARD);
+        grabber2.setDirection(Servo.Direction.REVERSE);
+
+
+        rDrive1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rDrive2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lDrive1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lDrive2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        elevator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        while (opModeIsActive()) {
+            /* //Drive/turn
+            if (gamepad1.right_stick_y > 0.2 || gamepad1.right_stick_y < -0.2) {
+                rDrive1.setPower(gamepad1.right_stick_y);
+                rDrive2.setPower(gamepad1.right_stick_y);
+            } else {
+                rDrive1.setPower(0);
+                rDrive2.setPower(0);
+            }
+            if (gamepad1.left_stick_y > 0.2 || gamepad1.left_stick_y < -0.2) {
+                lDrive1.setPower(gamepad1.left_stick_y);
+                lDrive2.setPower(gamepad1.left_stick_y);
+            } else {
+                lDrive1.setPower(0);
+                lDrive2.setPower(0);
+            }
+
+
+            //slide
+            if (gamepad1.right_trigger > 0) {
+                slide.setPower(-gamepad1.right_trigger);
+                telemetry.addData("Slide Power:", slide.getPower());
+                telemetry.update();
+            } else if (gamepad1.left_trigger > 0) {
+                slide.setPower(gamepad1.left_trigger);
+                telemetry.addData("Slide Power:", slide.getPower());
+                telemetry.update();
+            } else {
+                slide.setPower(0);
+                telemetry.addData("Slide Power:", slide.getPower());
+                telemetry.update();
+            }*/
+
+             //Drive gilad
+            if (gamepad1.left_stick_y > 0.2 || gamepad1.left_stick_y < -0.2) {
+                if (gamepad1.left_stick_y > 0.3 && gamepad1.left_stick_y < 0.4){
+                    rDrive1.setPower(gamepad1.left_stick_y - 0.1);
+                    rDrive2.setPower(gamepad1.left_stick_y - 0.1);
+                }else if (gamepad1.left_stick_y > 0.4 && gamepad1.left_stick_y < 0.5) {
+                    rDrive1.setPower(gamepad1.left_stick_y - 0.1);
+                    rDrive2.setPower(gamepad1.left_stick_y - 0.1);
+                }
+                if (gamepad1.left_stick_y < -0.3 && gamepad1.left_stick_y > -0.4){
+                    rDrive1.setPower(gamepad1.left_stick_y + 0.1);
+                    rDrive2.setPower(gamepad1.left_stick_y + 0.1);
+                }else if (gamepad1.left_stick_y < -0.4 && gamepad1.left_stick_y > -0.5) {
+                    rDrive1.setPower(gamepad1.left_stick_y + 0.1);
+                    rDrive2.setPower(gamepad1.left_stick_y + 0.1);
+                }
+                if (gamepad1.left_stick_y > 0.5){
+                    rDrive1.setPower(gamepad1.left_stick_y);
+                    rDrive2.setPower(gamepad1.left_stick_y);
+                    lDrive1.setPower(gamepad1.left_stick_y);
+                    lDrive2.setPower(gamepad1.left_stick_y);
+                }
+
+
+
+
+
+            } else if (gamepad1.left_trigger > 0.2) {
+                rDrive1.setPower(-gamepad1.left_trigger);
+                rDrive2.setPower(-gamepad1.left_trigger);
+                lDrive1.setPower(gamepad1.left_trigger);
+                lDrive2.setPower(gamepad1.left_trigger);
+
+            } else if (gamepad1.right_trigger > 0.2) {
+                rDrive1.setPower(gamepad1.right_trigger);
+                rDrive2.setPower(gamepad1.right_trigger);
+                lDrive1.setPower(-gamepad1.right_trigger);
+                lDrive2.setPower(-gamepad1.right_trigger);
+
+            }
+
+            else {
+                rDrive1.setPower(0);
+                rDrive2.setPower(0);
+                lDrive1.setPower(0);
+                lDrive2.setPower(0);
+            }
+
+            if (gamepad1.right_stick_x > 0 || gamepad1.right_stick_x < 0) {
+                slide.setPower(-gamepad1.right_stick_x);
+            } else {
+                slide.setPower(0);
+            }
+            telemetry.addData("rtrigger:", gamepad1.right_trigger);
+            telemetry.addData("ltrigger:", gamepad1.left_trigger);
+            telemetry.addData("drive:", rDrive1.getPower());
+            telemetry.update();
+//elevator
+            if (gamepad2.right_stick_y > 0.2) {
+                elevator.setPower(gamepad2.right_stick_y);
+            } else if (gamepad2.right_stick_y < 0.2) {
+                elevator.setPower(gamepad2.right_stick_y);
+            } else {
+                elevator.setPower(0);
+            }
+
+            if (gamepad2.right_stick_y > 0.2) {
+                elevator.setPower(gamepad2.right_stick_y);
+            } else if (gamepad2.right_stick_y < 0.2) {
+                elevator.setPower(gamepad2.right_stick_y);
+            } else {
+                elevator.setPower(0);
+            }
+
+
+            //collection
+            if (gamepad2.right_trigger > 0) {
+                collectRight.setPosition(0.7);
+                collectLeft.setPosition(0.2);
+            } else if (gamepad2.left_trigger > 0) {
+                collectLeft.setPosition(0.7);
+                collectRight.setPosition(0.2);
+
+            } else {
+                collectRight.setPosition(0);
+                collectLeft.setPosition(0);
+            }
+            //collection fold
+            if (gamepad2.right_bumper) {
+                foldcollect.setPower(1);
+            } else if (gamepad2.left_bumper) {
+                foldcollect.setPower(-1);
+            } else {
+                foldcollect.setPower(0);
+            }
+
+//grabber
+
+            if (gamepad2.x) {
+                grabber1.setPosition(50);
+                grabber2.setPosition(50);
+            } else {
+                grabber1.setPosition(0);
+                grabber2.setPosition(0);
+            }
+
+
+            //Drop cube on plate
+            if(gamepad1.a){
+                rDrive1.setPower(0.7);
+                rDrive2.setPower(0.7);
+                lDrive1.setPower(0.7);
+                lDrive2.setPower(0.7);
+
+                collectRight.setPosition(0.3);
+                collectLeft.setPosition(0.7);
+            }
+
+
+
+        }
+
+
+    }
+}
